eab5c68a76949027ea91ef9c608c3c43f7d58dab
Mukesh-cpu-gif <mukeshluvv7@gmail.com>
2025-11-22 21:04:46 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB5Zwe6e_INzSsF6EOAmb5Ey6wFoD_RFhQ",
     authDomain: "night-shift-cats.firebaseapp.com",
+    databaseURL: "https://night-shift-cats-default-rtdb.firebaseio.com",
     projectId: "night-shift-cats",
     storageBucket: "night-shift-cats.firebasestorage.app",
     messagingSenderId: "504970276694",
     appId: "1:504970276694:web:96b8e08269658c93abb0f7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
