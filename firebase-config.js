fae90dd2ea8e3e2f2303c1e6737d7bc7068759cd
Mukesh-cpu-gif <mukeshluvv7@gmail.com>
2025-11-22 21:17:39 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
-// Firebase Configuration
-// This file is loaded by index.html before game.js
+// Firebase Configuration (v8 syntax)
 const firebaseConfig = {
-    apiKey: "AIzaSyB5Zwe6e_INzSsF6EOAmb5Ey6wFoD_RFhQ",
-    authDomain: "night-shift-cats.firebaseapp.com",
-    databaseURL: "https://night-shift-cats-default-rtdb.firebaseio.com",
-    projectId: "night-shift-cats",
-    storageBucket: "night-shift-cats.firebasestorage.app",
-    messagingSenderId: "504970276694",
-    appId: "1:504970276694:web:96b8e08269658c93abb0f7"
+  apiKey: "AIzaSyB5Zwe6e_INzSsF6EOAmb5Ey6wFoD_RFhQ",
+  authDomain: "night-shift-cats.firebaseapp.com",
+  databaseURL: "https://night-shift-cats-default-rtdb.firebaseio.com",
+  projectId: "night-shift-cats",
+  storageBucket: "night-shift-cats.firebasestorage.app",
+  messagingSenderId: "504970276694",
+  appId: "1:504970276694:web:96b8e08269658c93abb0f7"
 };
-// game.js will use this global variable to initialize Firebase
-console.log("Firebase Config Loaded");
+
+firebase.initializeApp(firebaseConfig);
+console.log("Firebase v8 initialized");
